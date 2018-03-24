7482e9d88bdff551d8d254d9e33a288cfb2948a4
Luke Mwila <outlier.developer@gmail.com>
2018-03-24 13:36:41 +0200

    Instaled additional dependcies for the backend and created a server.js file to start our nodejs express web server

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(4000, () => console.log('Express server running on port 4000'))
